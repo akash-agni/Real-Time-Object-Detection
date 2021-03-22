72d4c3fa1791a4b33f4e6149fbe20651f70b7a59
Akash Agnihotri <akashagni26@gmail.com>
2021-03-23 01:14:31 +0530

    Create Object_Detection_Youtube.py

---
+++ Object_Detection_Youtube.py
@@ -0,0 +1,72 @@
+import torch
+import numpy as np
+import cv2
+import pafy
+from time import time
+
+
+class ObjectDetection:
+
+    def __init__(self, url, out_file="Labeled_Video.avi"):
+        self._URL = url
+        self.model = self.load_model()
+        self.classes = self.model.names
+        self.out_file = out_file
+        self.device = 'cuda' if torch.cuda.is_available() else 'cpu'
+
+    def get_video_from_url(self):
+        play = pafy.new(self._URL).streams[-1]
+        assert play is not None
+        return cv2.VideoCapture(play.url)
+
+
+    def load_model(self):
+        model = torch.hub.load('ultralytics/yolov5', 'yolov5s', pretrained=True)
+        return model
+
+    def score_frame(self, frame):
+        self.model.to(self.device)
+        frame = [torch.tensor(frame)]
+        results = self.model(frame)
+        labels, cord = results.xyxyn[0][:, -1].numpy(), results.xyxyn[0][:, :-1].numpy()
+        return labels, cord
+
+    def class_to_label(self, x):
+        return self.classes[int(x)]
+
+    def plot_boxes(self, results, frame):
+        labels, cord = results
+        n = len(labels)
+        x_shape, y_shape = frame.shape[1], frame.shape[0]
+        for i in range(n):
+            row = cord[i]
+            if row[4] >= 0.2:
+                x1, y1, x2, y2 = int(row[0]*x_shape), int(row[1]*y_shape), int(row[2]*x_shape), int(row[3]*y_shape)
+                bgr = (0, 255, 0)
+                cv2.rectangle(frame, (x1, y1), (x2, y2), bgr, 2)
+                cv2.putText(frame, self.class_to_label(labels[i]), (x1, y1), cv2.FONT_HERSHEY_SIMPLEX, 0.9, bgr, 2)
+
+        return frame
+
+    def __call__(self):
+        player = self.get_video_from_url()
+        assert player.isOpened()
+        x_shape = int(player.get(cv2.CAP_PROP_FRAME_WIDTH))
+        y_shape = int(player.get(cv2.CAP_PROP_FRAME_HEIGHT))
+        four_cc = cv2.VideoWriter_fourcc(*"MJPG")
+        out = cv2.VideoWriter(self.out_file, four_cc, 20, (x_shape, y_shape))
+        while True:
+            start_time = time()
+            ret, frame = player.read()
+            assert ret
+            results = self.score_frame(frame)
+            frame = self.plot_boxes(results, frame)
+            end_time = time()
+            fps = 1/np.round(end_time - start_time, 3)
+            print(f"Frames Per Second : {fps}")
+            out.write(frame)
+
+
+a = ObjectDetection("https://www.youtube.com/watch?v=dwD1n7N7EAg")
+a()
+
